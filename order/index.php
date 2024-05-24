56329676095c58e0744f0dba16b38a81eda1d832
Deny Kurnia Hermawan <denykurnia040@gmail>
2024-05-24 21:20:49 +0700

    jjj

--- order/index.php
+++ order/index.php
@@ -1,14 +1,17 @@
-<?php require_once('../_functions.php');?>
+<?php require_once('../_functions.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Redirect</title>
+   <title>Redirect L</title>
 </head>
+
 <body>
    <script>
-      window.location='<?=url('order/order.php');?>';
+      window.location = '<?= url('order/order.php'); ?>';
    </script>
 </body>
+
 </html>
